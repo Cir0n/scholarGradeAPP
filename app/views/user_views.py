47d15bff8806c1a7c3d658d141284cf3936692f9
Cir0n <philippe.baba3000@gmail.com>
2025-01-18 16:56:19 +0100

    login view

--- app/views/user_views.py
+++ app/views/user_views.py
@@ -15,35 +15,29 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        # Récupérer l'utilisateur par nom d'utilisateur
         user_data = get_user_by_username(username)
-        print("DEBUG - user_data:", user_data)  # Ajoute ce log pour vérifier les données
 
         if not user_data or 'password_hash' not in user_data:
             error_message = "Nom d'utilisateur ou mot de passe incorrect"
             return render_template('login.html', error=error_message)
 
-        # Vérification du mot de passe
         if not bcrypt.check_password_hash(user_data['password_hash'], password):
             error_message = "Nom d'utilisateur ou mot de passe incorrect"
             return render_template('login.html', error=error_message)
 
-        # Connecter l'utilisateur
         user = User(user_data['id'], user_data['username'], user_data['role'])
         login_user(user)
-        return redirect('/users')  # Rediriger vers la liste des utilisateurs
-
-    # Si méthode GET, afficher le formulaire de connexion
+        return redirect('/users') 
     return render_template('login.html')
 
 
 
-# Route de déconnexion
 @user_bp.route('/logout', methods=['GET'])
 @login_required
 def logout():
     logout_user()
     return jsonify({"message": "Logged out successfully"}), 200
+
 
 @user_bp.route('/dashboard', methods=['GET'])
 @login_required
